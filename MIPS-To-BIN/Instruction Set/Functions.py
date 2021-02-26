192d1cad715fbaf47389064fa270fd3f5246f3dc
Jeremias-V <jeremias.villalobos.t@gmail.com>
2021-02-26 16:32:06 -0500

    Instruction Set

---
+++ MIPS-To-BIN/Instruction Set/Functions.py
@@ -0,0 +1,12 @@
+import os
+
+def getFunctions():
+    functions = dict()
+    cur_path = os.path.dirname(__file__)
+    path = cur_path + 'Instructions/Functions.txt'
+    with open(path, 'r') as f:
+        instructions = f.read().split('\n')
+    for i in instructions:
+        currentIns = i.split()
+        functions[currentIns[0]] = currentIns[1]
+    return functions
---
+++ MIPS-To-BIN/Instruction Set/Registers.py
@@ -0,0 +1,12 @@
+import os
+
+def getRegisters():
+    registers = dict()
+    cur_path = os.path.dirname(__file__)
+    path = cur_path + 'Instructions/Registers.txt'
+    with open(path, 'r') as f:
+        instructions = f.read().split('\n')
+    for i in instructions:
+        currentIns = i.split()
+        registers[currentIns[0]] = currentIns[1]
+    return registers
